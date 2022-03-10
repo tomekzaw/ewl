39d855a60b816cb6e022ebcb7f13bb56eab58c85
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-03-10 21:54:03 +0100

    Improve parametrizations (#21)
    * Move parametrizations to a separate file
    
    * Improve unit tests of parametrizations
    
    * Add Frąckiewicz-Pykacz parametrization
    
    * Alias sympy functions
    
    * Refactor original EWL parametrization
    
    * Add another parametrization
    
    * Add general parametrization from Qiskit notebook
    
    * Fix typo
    
    * Fix linter
    
    * Add another parametrization
    
    * Reorder definitions

---
+++ src/ewl/parametrizations.py
@@ -0,0 +1,126 @@
+import sympy as sp
+from sympy import Matrix
+
+i = sp.I
+pi = sp.pi
+sin = sp.sin
+cos = sp.cos
+exp = sp.exp
+
+
+def U_theta_alpha_beta(*, theta, alpha, beta) -> Matrix:
+    """
+    Full SU(2) parametrization from "A quantum approach to twice-repeated 2×2 game"
+    by Katarzyna Rycerz and Piotr Frąckiewicz (DOI:10.1007/s11128-020-02743-0).
+    https://www.researchgate.net/publication/343293505_A_quantum_approach_to_twice-repeated_2times_2_game
+    :param theta:
+    :param alpha:
+    :param beta:
+    :return:
+    """
+    return Matrix([
+        [exp(i * alpha) * cos(theta / 2), i * exp(i * beta) * sin(theta / 2)],
+        [i * exp(-i * beta) * sin(theta / 2), exp(-i * alpha) * cos(theta / 2)]
+    ])
+
+
+def U_theta_gamma_delta(*, theta, gamma, delta) -> Matrix:
+    """
+    Full SU(2) parametrization from "A quantum approach to twice-repeated 2×2 game"
+    by Katarzyna Rycerz and Piotr Frąckiewicz (DOI:10.1007/s11128-020-02743-0).
+    https://www.researchgate.net/publication/343293505_A_quantum_approach_to_twice-repeated_2times_2_game
+    :param theta:
+    :param gamma:
+    :param delta:
+    :return:
+    """
+    return Matrix([
+        [exp(i * (gamma + delta)) * cos(theta / 2), i * exp(i * (gamma - delta)) * sin(theta / 2)],
+        [i * exp(-i * (gamma - delta)) * sin(theta / 2), exp(-i * (gamma + delta)) * cos(theta / 2)]
+    ])
+
+
+def U_theta_phi_lambda(*, theta, phi, lambda_) -> Matrix:
+    """
+    Full SU(2) parametrization.
+    :param theta:
+    :param phi:
+    :param lambda_:
+    :return:
+    """
+    return Matrix([
+        [exp(-i * (phi + lambda_) / 2) * cos(theta / 2), -exp(-i * (phi - lambda_) / 2) * sin(theta / 2)],
+        [exp(i * (phi - lambda_) / 2) * sin(theta / 2), exp(i * (phi + lambda_) / 2) * cos(theta / 2)]
+    ])
+
+
+def U_theta_phi_alpha(*, theta, phi, alpha) -> Matrix:
+    """
+    Parametrization used in "Dlaczego w dylemat więźnia warto grać kwantowo?" by Marek Szopa.
+    https://www.ue.katowice.pl/fileadmin/_migrated/content_uploads/11_M.Szopa__Dlaczego_w_dylemat_wieznia....pdf
+    :param theta: [0, PI]
+    :param phi: [-PI, PI]
+    :param alpha: [-PI, PI]
+    :return:
+    """
+    return Matrix([
+        [exp(-i * phi) * cos(theta / 2), exp(i * alpha) * sin(theta / 2)],
+        [-exp(-i * alpha) * sin(theta / 2), exp(i * phi) * cos(theta / 2)]
+    ])
+
+
+def U_IBM(*, theta, phi, lambda_) -> Matrix:
+    """
+    General single-qubit quantum gate as defined in Qiskit textbook by IBM.
+    https://qiskit.org/textbook/ch-states/single-qubit-gates.html#7.-The-U-gate--
+    :param theta:
+    :param phi:
+    :param lambda_:
+    :return:
+    """
+    return Matrix([
+        [cos(theta / 2), -exp(i * lambda_) * sin(theta / 2)],
+        [exp(i * phi) * sin(theta / 2), exp(i * (phi + lambda_)) * cos(theta / 2)]
+    ])
+
+
+def U_Eisert_Wilkens_Lewenstein(*, theta, phi) -> Matrix:
+    """
+    Original parametrization from "Quantum Games and Quantum Strategies"
+    by Jens Eisert, Martin Wilkens, and Maciej Lewenstein (DOI:10.1103/PhysRevLett.83.3077).
+    https://arxiv.org/pdf/quant-ph/9806088.pdf
+    :param theta: [0, PI]
+    :param phi: [0, PI/2]
+    :return:
+    """
+    return Matrix([
+        [exp(i * phi) * cos(theta / 2), sin(theta / 2)],
+        [-sin(theta / 2), exp(-i * phi) * cos(theta / 2)]
+    ])
+
+
+def U_Frackiewicz_Pykacz(*, theta, phi) -> Matrix:
+    """
+    Full SU(2) parametrization from "Quantum Games with Strategies Induced by Basis Change Rules"
+    by Piotr Frąckiewicz and Jarosław Pykacz (DOI:10.1007/s10773-017-3423-6).
+    https://link.springer.com/content/pdf/10.1007%2Fs10773-017-3423-6.pdf
+    :param theta: [0, PI]
+    :param phi: [0, 2*PI]
+    :return:
+    """
+    return Matrix([
+        [exp(i * phi) * cos(theta / 2), i * exp(i * phi) * sin(theta / 2)],
+        [i * exp(-i * phi) * sin(theta / 2), exp(-i * phi) * cos(theta / 2)]
+    ])
+
+
+def U(*args, **kwargs) -> Matrix:
+    if args:
+        raise Exception('Please use keyword arguments')
+    if set(kwargs) == {'theta', 'alpha', 'beta'}:
+        return U_theta_alpha_beta(**kwargs)
+    if set(kwargs) == {'theta', 'phi', 'lambda_'}:
+        return U_theta_phi_lambda(**kwargs)
+    if set(kwargs) == {'theta', 'phi'}:
+        return U_Eisert_Wilkens_Lewenstein(**kwargs)
+    raise Exception('Invalid parametrization')
---
+++ tests/test_parametrizations.py
@@ -0,0 +1,104 @@
+from typing import Dict
+
+import pytest
+import sympy as sp
+from sympy import Matrix
+
+from ewl.parametrizations import U_theta_alpha_beta, U_theta_gamma_delta, U_theta_phi_lambda, U_theta_phi_alpha, \
+    U_Eisert_Wilkens_Lewenstein, U_Frackiewicz_Pykacz, U_IBM, U
+
+i = sp.I
+pi = sp.pi
+sqrt2 = sp.sqrt(2)
+sin = sp.sin
+cos = sp.cos
+exp = sp.exp
+
+
+def test_U_theta_alpha_beta():
+    theta, alpha, beta = sp.symbols('theta alpha beta')
+    actual = U_theta_alpha_beta(theta=theta, alpha=alpha, beta=beta)
+    expected = Matrix([
+        [exp(i * alpha) * cos(theta / 2), i * exp(i * beta) * sin(theta / 2)],
+        [i * exp(-i * beta) * sin(theta / 2), exp(-i * alpha) * cos(theta / 2)]
+    ])
+    assert actual == expected
+
+
+def test_U_theta_gamma_delta():
+    theta, gamma, delta = sp.symbols('theta gamma delta')
+    actual = U_theta_gamma_delta(theta=theta, gamma=gamma, delta=delta)
+    expected = Matrix([
+        [exp(i * (gamma + delta)) * cos(theta / 2), i * exp(i * (gamma - delta)) * sin(theta / 2)],
+        [i * exp(-i * (gamma - delta)) * sin(theta / 2), exp(-i * (gamma + delta)) * cos(theta / 2)]
+    ])
+    assert actual == expected
+
+
+def test_U_theta_phi_lambda():
+    theta, phi, lambda_ = sp.symbols('theta phi lambda')
+    actual = U_theta_phi_lambda(theta=theta, phi=phi, lambda_=lambda_)
+    expected = Matrix([
+        [exp(-i * (phi + lambda_) / 2) * cos(theta / 2), -exp(-i * (phi - lambda_) / 2) * sin(theta / 2)],
+        [exp(i * (phi - lambda_) / 2) * sin(theta / 2), exp(i * (phi + lambda_) / 2) * cos(theta / 2)]
+    ])
+    assert actual == expected
+
+
+def test_U_theta_phi_alpha():
+    theta, phi, alpha = sp.symbols('theta phi alpha')
+    actual = U_theta_phi_alpha(theta=theta, phi=phi, alpha=alpha)
+    expected = Matrix([
+        [exp(-i * phi) * cos(theta / 2), exp(i * alpha) * sin(theta / 2)],
+        [-exp(-i * alpha) * sin(theta / 2), exp(i * phi) * cos(theta / 2)]
+    ])
+    assert actual == expected
+
+
+def test_U_IBM():
+    theta, phi, lambda_ = sp.symbols('theta phi lambda')
+    actual = U_IBM(theta=theta, phi=phi, lambda_=lambda_)
+    expected = Matrix([
+        [cos(theta / 2), -exp(i * lambda_) * sin(theta / 2)],
+        [exp(i * phi) * sin(theta / 2), exp(i * (phi + lambda_)) * cos(theta / 2)]
+    ])
+    assert actual == expected
+
+
+def test_U_Eisert_Wilkens_Lewenstein():
+    theta, phi = sp.symbols('theta phi')
+    actual = U_Eisert_Wilkens_Lewenstein(theta=theta, phi=phi)
+    expected = Matrix([
+        [exp(i * phi) * cos(theta / 2), sin(theta / 2)],
+        [-sin(theta / 2), exp(-i * phi) * cos(theta / 2)]
+    ])
+    assert actual == expected
+
+
+def test_U_Frackiewicz_Pykacz():
+    theta, phi = sp.symbols('theta phi')
+    actual = U_Frackiewicz_Pykacz(theta=theta, phi=phi)
+    expected = Matrix([
+        [exp(i * phi) * cos(theta / 2), i * exp(i * phi) * sin(theta / 2)],
+        [i * exp(-i * phi) * sin(theta / 2), exp(-i * phi) * cos(theta / 2)]
+    ])
+    assert actual == expected
+
+
+@pytest.mark.parametrize('kwargs, expected', [
+    # theta, alpha, beta
+    (dict(theta=0, alpha=0, beta=pi / 2), Matrix([[1, 0], [0, 1]])),
+    (dict(theta=pi / 2, alpha=pi / 2, beta=0), Matrix([[i / sqrt2, i / sqrt2], [i / sqrt2, -i / sqrt2]])),
+    (dict(theta=0, alpha=pi / 2, beta=3 * pi / 2), Matrix([[i, 0], [0, -i]])),
+    (dict(theta=0, alpha=0, beta=0), Matrix([[1, 0], [0, 1]])),
+    (dict(theta=pi, alpha=0, beta=0), Matrix([[0, i], [i, 0]])),
+
+    # theta, phi, lambda
+    (dict(theta=0, phi=0, lambda_=0), Matrix([[1, 0], [0, 1]])),  # C
+    (dict(theta=pi, phi=0, lambda_=0), Matrix([[0, -1], [1, 0]])),  # D
+
+    # theta, phi
+    (dict(theta=0, phi=pi / 2), Matrix([[i, 0], [0, -i]]))
+])
+def test_U(kwargs: Dict[str, complex], expected: Matrix):
+    assert U(**kwargs) == expected
