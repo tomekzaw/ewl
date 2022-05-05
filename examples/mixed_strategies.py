a25a0ac927dfae75490f83351d3565c7dde57da5
Piotr Kotara <piotrekkotara@gmail.com>
2022-05-06 00:29:56 +0200

    Fix best strategies

--- examples/mixed_strategies.py
+++ examples/mixed_strategies.py
@@ -31,9 +31,9 @@ if __name__ == '__main__':
     theta_A, phi_A, alpha_A = sp.symbols('theta_A phi_A alpha_A', real=True)
 
     A_hat = U_theta_phi_alpha(theta=theta_A, phi=phi_A, alpha=alpha_A)
-    B_hat = U_theta_phi_alpha(theta=theta_A + pi, phi=alpha_A, alpha=phi_A - pi / 2)
-    A_hat_prim = U_theta_phi_alpha(theta=theta_A, phi=phi_A - pi / 2, alpha=alpha_A - pi / 2)
-    B_hat_prim = U_theta_phi_alpha(theta=theta_A + pi, phi=alpha_A - pi / 2, alpha=phi_A - pi)
+    B_hat = U_theta_phi_alpha(theta=theta_A + pi, phi=-alpha_A - pi/2, alpha=-phi_A)
+    A_hat_prim = U_theta_phi_alpha(theta=theta_A, phi=phi_A - pi/2, alpha=alpha_A + pi/2)
+    B_hat_prim = U_theta_phi_alpha(theta=-theta_A + pi, phi=-alpha_A, alpha=-phi_A + pi/2)
 
     gamma_A, gamma_B = sp.symbols('gamma_A gamma_B', real=True)
 
@@ -51,5 +51,7 @@ if __name__ == '__main__':
     mixed_ewl_fixed = mixed_ewl.fix(theta_A=pi / 2, phi_A=0, alpha_A=0)
 
     alice_payoff_function = mixed_ewl_fixed.payoff_function(player=0)
+
+    print(alice_payoff_function)
 
     plot3d(alice_payoff_function, (gamma_A, 0, pi), (gamma_B, 0, pi))
