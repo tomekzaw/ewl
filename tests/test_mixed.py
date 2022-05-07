32bb927f84d3ed9b4d7f619403fc7b46c2132447
Piotr Kotara <piotrekkotara@gmail.com>
2022-05-07 18:38:11 +0200

    added tests of mixed ewl

--- tests/test_mixed.py
+++ tests/test_mixed.py
@@ -4,7 +4,7 @@ import pytest
 import sympy as sp
 from sympy.physics.quantum.qubit import Qubit
 
-from ewl.mixed import MixedEWL, MixedStrategy
+from ewl.mixed import MixedEWL, MixedStrategy, kraus
 from ewl.parametrizations import U_theta_phi_alpha
 
 i = sp.I
@@ -29,9 +29,9 @@ payoff_matrix = sp.Array([
 theta_A, phi_A, alpha_A = sp.symbols('theta_A phi_A alpha_A', real=True)
 
 A_hat = U_theta_phi_alpha(theta=theta_A, phi=phi_A, alpha=alpha_A)
-B_hat = U_theta_phi_alpha(theta=theta_A + pi, phi=alpha_A, alpha=phi_A - pi / 2)
-A_hat_prim = U_theta_phi_alpha(theta=theta_A, phi=phi_A - pi / 2, alpha=alpha_A - pi / 2)
-B_hat_prim = U_theta_phi_alpha(theta=theta_A + pi, phi=alpha_A - pi / 2, alpha=phi_A - pi)
+B_hat = U_theta_phi_alpha(theta=theta_A + pi, phi=-alpha_A - pi/2, alpha=-phi_A)
+A_hat_prim = U_theta_phi_alpha(theta=theta_A, phi=phi_A - pi/2, alpha=alpha_A + pi/2)
+B_hat_prim = U_theta_phi_alpha(theta=-theta_A + pi, phi=-alpha_A, alpha=-phi_A + pi/2)
 
 gamma_A, gamma_B = sp.symbols('gamma_A gamma_B', real=True)
 
@@ -65,8 +65,11 @@ def mixed_ewl_fixed(mixed_ewl: MixedEWL) -> MixedEWL:
 
 
 def test_kraus() -> None:
-    # TODO: implement
-    pass
+    assert D == kraus(C, D)
+    assert C == kraus(D, C)
+
+    M = sp.Matrix([[0, 1], [1, 0]])
+    assert M == kraus(D, M)
 
 
 def test_MixedStrategy_invalid_sum() -> None:
@@ -80,8 +83,7 @@ def test_MixedStrategy_params(alice: MixedStrategy, bob: MixedStrategy) -> None:
 
 
 def test_MixedStrategy_fix(alice: MixedStrategy) -> None:
-    # TODO: implement
-    pass
+    assert not alice.fix(theta_A=pi/2, phi_A=0, alpha_A=0, gamma_A=sp.sympify('1/2')).params
 
 
 def test_MixedEWL_number_of_players(mixed_ewl: MixedEWL) -> None:
@@ -93,9 +95,31 @@ def test_MixedEWL_params(mixed_ewl: MixedEWL, mixed_ewl_fixed: MixedEWL) -> None
     assert mixed_ewl_fixed.params == {gamma_A, gamma_B}
 
 
-def test_MixedEWL_fix(mixed_ewl: MixedEWL) -> None:
-    # TODO: implement
-    pass
+def test_MixedEWL_fix(mixed_ewl: MixedEWL, mixed_ewl_fixed: MixedEWL) -> None:
+    theta_A_fix, phi_A_fix, alpha_A_fix = pi / 2, 0, 0
+
+    A_hat_fix = U_theta_phi_alpha(theta=theta_A_fix, phi=phi_A_fix, alpha=alpha_A_fix)
+    B_hat_fix = U_theta_phi_alpha(theta=theta_A_fix + pi, phi=alpha_A_fix, alpha=phi_A_fix - pi / 2)
+    A_hat_prim_fix = U_theta_phi_alpha(theta=theta_A_fix, phi=phi_A_fix - pi / 2, alpha=alpha_A_fix - pi / 2)
+    B_hat_prim_fix = U_theta_phi_alpha(theta=theta_A_fix + pi, phi=alpha_A_fix - pi / 2, alpha=phi_A_fix - pi)
+
+    gamma_A, gamma_B = sp.symbols('gamma_A gamma_B', real=True)
+
+    alice_fixed: MixedStrategy = MixedStrategy([
+            (sp.cos(gamma_A / 2) ** 2, A_hat_fix),
+            (sp.sin(gamma_A / 2) ** 2, A_hat_prim_fix),
+        ])
+
+    bob_fixed: MixedStrategy = MixedStrategy([
+            (sp.cos(gamma_B / 2) ** 2, B_hat_fix),
+            (sp.sin(gamma_B / 2) ** 2, B_hat_prim_fix),
+        ])
+
+    manually_fixed_ewl = MixedEWL(psi=psi, C=C, D=D, players=[alice_fixed, bob_fixed], payoff_matrix=payoff_matrix)
+
+    assert mixed_ewl_fixed.psi == manually_fixed_ewl.psi
+    assert mixed_ewl_fixed.players[0] == mixed_ewl_fixed.players[0]
+    assert mixed_ewl_fixed.players[1] == mixed_ewl_fixed.players[1]
 
 
 def test_MixedEWL_amplitudes(mixed_ewl: MixedEWL) -> None:
@@ -103,14 +127,21 @@ def test_MixedEWL_amplitudes(mixed_ewl: MixedEWL) -> None:
         mixed_ewl.amplitudes()
 
 
-def test_MixedEWL_density_matrix(mixed_ewl: MixedEWL) -> None:
-    # TODO: implement
-    pass
+def test_MixedEWL_density_matrix(mixed_ewl_fixed: MixedEWL) -> None:
+    alice_payoff_function = mixed_ewl_fixed.payoff_function(player=0)
+
+    print(mixed_ewl_fixed.density_matrix)
+    expected_payoff = 5*sp.cos(gamma_A - gamma_B)/4 + 5*sp.cos(gamma_A + gamma_B)/4 + sp.sympify(5/2)
+    assert sp.simplify(alice_payoff_function - expected_payoff) == 0
 
 
 def test_MixedEWL_probs(mixed_ewl: MixedEWL) -> None:
-    # TODO: implement
-    pass
+    expectedProbs = sp.Matrix([
+        [0, 
+         -sp.cos(gamma_A - gamma_B)/4 - sp.cos(gamma_A + gamma_B)/4 + 1/2,
+         sp.cos(gamma_A - gamma_B)/4 + sp.cos(gamma_A + gamma_B)/4 + 1/2,
+         0]])
+    assert sp.simplify(mixed_ewl.probs() - expectedProbs) == sp.Matrix([[0, 0, 0, 0]])
 
 
 def test_MixedEWL_probs_sum(mixed_ewl_fixed: MixedEWL) -> None:
@@ -118,5 +149,5 @@ def test_MixedEWL_probs_sum(mixed_ewl_fixed: MixedEWL) -> None:
 
 
 def test_MixedEWL_payoff_function(mixed_ewl: MixedEWL) -> None:
-    # TODO: implement
-    pass
+    assert sp.simplify(mixed_ewl.payoff_function(player=0) - (5*sp.cos(gamma_A - gamma_B)/4 + 5*sp.cos(gamma_A + gamma_B)/4 + 5/2)) == 0
+    assert sp.simplify(mixed_ewl.payoff_function(player=1) - (-5*sp.cos(gamma_A - gamma_B)/4 - 5*sp.cos(gamma_A + gamma_B)/4 + 5/2)) == 0
