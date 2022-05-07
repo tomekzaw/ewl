be5913be2549897131e8ba99478845f74a92a0e7
Piotr Kotara <piotrekkotara@gmail.com>
2022-05-07 18:53:57 +0200

    fix linter errors

--- examples/mixed_strategies.py
+++ examples/mixed_strategies.py
@@ -31,9 +31,9 @@ if __name__ == '__main__':
     theta_A, phi_A, alpha_A = sp.symbols('theta_A phi_A alpha_A', real=True)
 
     A_hat = U_theta_phi_alpha(theta=theta_A, phi=phi_A, alpha=alpha_A)
-    B_hat = U_theta_phi_alpha(theta=theta_A + pi, phi=-alpha_A - pi/2, alpha=-phi_A)
-    A_hat_prim = U_theta_phi_alpha(theta=theta_A, phi=phi_A - pi/2, alpha=alpha_A + pi/2)
-    B_hat_prim = U_theta_phi_alpha(theta=-theta_A + pi, phi=-alpha_A, alpha=-phi_A + pi/2)
+    B_hat = U_theta_phi_alpha(theta=theta_A + pi, phi=-alpha_A - pi / 2, alpha=-phi_A)
+    A_hat_prim = U_theta_phi_alpha(theta=theta_A, phi=phi_A - pi / 2, alpha=alpha_A + pi / 2)
+    B_hat_prim = U_theta_phi_alpha(theta=-theta_A + pi, phi=-alpha_A, alpha=-phi_A + pi / 2)
 
     gamma_A, gamma_B = sp.symbols('gamma_A gamma_B', real=True)
 
